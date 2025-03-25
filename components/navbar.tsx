c9d6507e1e9131335daf3fdc4cc1e3c99b5e0fe9
4NG3LV4RG4S <juan_vargas@autorefax.com>
2025-03-25 00:24:19 -0600

    changes add url images blob storage

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,6 +5,7 @@ import Link from "next/link"
 import { Menu } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import Image from "next/image"
 
 export default function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false)
@@ -26,15 +27,20 @@ export default function Navbar() {
 
   return (
     <header
-      className={`w-full fixed top-0 z-50 transition-all duration-300 ${
-        isScrolled ? "bg-[#f7f3e9]/95 backdrop-blur-sm shadow-sm" : "bg-[#f7f3e9]"
-      }`}
+      className={`w-full fixed top-0 z-50 transition-all duration-300 ${isScrolled ? "bg-[#f7f3e9]/95 backdrop-blur-sm shadow-sm" : "bg-[#f7f3e9]"
+        }`}
     >
       <div className="container mx-auto px-4">
         <div className="h-16 flex items-center justify-between">
           <div className="flex items-center">
-            <Link href="/" className="text-[#9c7a5b] font-bold text-xl md:text-2xl">
-              Café Especial
+            <Link href="/" className="text-[#9c7a5b] font-bold flex items-center space-x-2 text-xl md:text-2xl">
+              <Image
+                src="https://caracolillo.blob.core.windows.net/media-web-caracolillo/CaracolilloMediaImage/2024-10-09%2010%2045%2049.png"
+                alt="Logo Caracolillo"
+                width={95}
+                height={52} />
+
+              Caracolillo
             </Link>
           </div>
 
